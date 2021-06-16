145cc2e8a2fcfd8df7b18515a1f1ea58a89e6d37
金妍廷 <kinyona@yo.local>
2021-06-17 03:56:49 +0900

    0617

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
-$(window).load(function() {
-	var img = $("#slideshow").children("img"), // 画像を取得
-		num = img.length, // 画像の数を数える
-		count = 0, // 現在何枚目を表示しているかのカウンター
-		interval = 5000; // 次の画像に切り替わるまでの時間(1/1000秒)
-	
-	img.eq(0).addClass("show"); // 最初の画像にshowクラス付与
-	
-	setTimeout(slide, interval); // slide関数をタイマーセット
-	
-	function slide() {
-		img.eq(count).removeClass("show"); // 現在表示している画像からshowクラスを取り除く
-		count++; // カウンターを一個進める
-		if(count >= num) {
-			count = 0; // カウンターが画像の数より大きければリセット
-		}
-		img.eq(count).addClass("show"); // 次の画像にshowクラス付与
-		setTimeout(slide, interval); // 再びタイマーセット
-	}
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        mode:'fade', 
+        auto: true ,
+        pause: 5000,
+        speed: 3000,
+        slideMargin: 5,
+        pager: false,
+        controls: false});
+  });
+
+  $(function () {
+  $(window).on("scroll", function () {
+  const sliderHeight = $(".color-change").height();
+  if (sliderHeight - 30 < $(this).scrollTop()) {
+      $(".scrolldown span").addClass("ColorScroll");
+      $(".scrolldown:before").addClass("ColorScroll");
+      $(".scrolldown:after").addClass("ColorScroll");
+
+  } else {
+      $(".scrolldown span").removeClass("ColorScroll");
+      $(".scrolldown:before").removeClass("ColorScroll")
+      $(".scrolldown:after").removeClass("ColorScroll")
+  }
+  });
+  });
+
+  $('a[href*="#"]').click(function () {
+	var elmHash = $(this).attr('href'); 
+	var pos = $(elmHash).offset().top;	
+	$('body,html').animate({scrollTop: pos}, 1500); 
+	return false;
 });
+
+
+
